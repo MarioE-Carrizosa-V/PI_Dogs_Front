e06f6a1164d8f3d3b37a98280da6d18e02ad49c8
Mario E Carrizosa Villarreal <mytcarrizosaland@gmail.com>
2023-07-01 10:19:31 -0600

    Deploy

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom'
+import store from './redux/store'
+import { Provider } from 'react-redux'
+import axios from 'axios'
+
+//axios.defaults.baseURL = 'http://localhost:3001/'
+axios.defaults.baseURL = 'https://back-wjik.onrender.com/'
+
+ReactDOM.render(
+  
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+  
+);
